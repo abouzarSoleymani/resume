87697ce21ad8faad40bf4e2a463b2131565412ab
abouzarSoleymani <iras.user@gmail.com>
2019-10-03 20:03:18 +0330

    Updates

---
+++ precache-manifest.740c32f5f702b8c09fda52c02d717655.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aac636adc4379da5f8a23d8bbcd5315",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "d483a34b7869b7980993",
+    "url": "/resume/static/css/main.3cb0ceaf.chunk.css"
+  },
+  {
+    "revision": "4c7e647befdc45c2bdcc",
+    "url": "/resume/static/js/2.a1f82b8a.chunk.js"
+  },
+  {
+    "revision": "d483a34b7869b7980993",
+    "url": "/resume/static/js/main.cbe5ac26.chunk.js"
+  },
+  {
+    "revision": "a25015690c8386b0270a",
+    "url": "/resume/static/js/runtime-main.28064bc1.js"
+  },
+  {
+    "revision": "4c732369e54ee14c3d884b15b441ad0f",
+    "url": "/resume/static/media/Angular.4c732369.svg"
+  },
+  {
+    "revision": "b848d5d9bb221592064de0f356f61676",
+    "url": "/resume/static/media/CSS-3.b848d5d9.png"
+  },
+  {
+    "revision": "063d129e7aa53dd090157bbadc88edd2",
+    "url": "/resume/static/media/Javascript.063d129e.png"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/resume/static/media/React-js.47ce6e77.png"
+  }
+]);
